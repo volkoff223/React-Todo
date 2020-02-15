b51ed5346b8fb26ddebf351869d2c9a82de091d7
Nicholas Voelker <volkoff223@gmail.com>
2020-02-14 21:20:39 -0500

    Converting to a pre RV trip check list

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header style={headerStyle}>
-            <h1>TodoList</h1>
+            <h1>FIFTH WHEEL CHECK LIST</h1>
             <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
         </header>
     )
--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <React.Fragment>
             <h1>About</h1>
-            <p>This is the TodoList app v1.0.0.</p>
+            <p>This is the Fifth Wheel Check List app v1.0.0.</p>
             
         </React.Fragment>
     )
